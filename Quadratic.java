88e7692af5f4cbc02bca64c607d804c3496656ce
Owen Coyle <ocoyle@thacher.org>
2018-10-07 01:20:17 -0700

    Add files via upload

---
+++ Quadratic.java
@@ -0,0 +1,14 @@
+/*
+What does this file do?
+
+Author: Who made it?
+
+Last Revision: When?
+
+Help Given/Received: What? Who?
+*/
+
+public class Quadratic{
+   public static void main(String[] args){
+   }
+}
